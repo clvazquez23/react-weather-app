1a50ada5aa198712d9464f3b07f9ef3fc1bb826b
clvazquez23 <clvazquez23@gmail.com>
2020-09-30 11:26:09 -0500

    updated github link

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
 import React, { Component } from "react";
 import "bootstrap/dist/css/bootstrap.css";
-import Navigation from "./Navigation";
 import Main from "./Main";
 
 export default class App extends Component {
   render() {
     return (
       <div>
-        <Navigation />
         <Main />
       </div>
     );
